e446b1a6ec3855b96ccd34b2f05a49e78672a61c
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-12-14 16:47:22 +0100

    Commit

--- src/main/kotlin/Report.kt
+++ src/main/kotlin/Report.kt
@@ -1,6 +1,5 @@
 package com.example.citylife.model.report
 
-import android.location.Location
 import java.time.LocalDateTime
 
 /**
@@ -11,5 +10,5 @@ import java.time.LocalDateTime
  * testo,
  * username dell'utente che la invia
  */
-data class Report(val type: ReportType, val location: Location,
+data class Report(val type: ReportType, val location: String,
                   val localDateTime: LocalDateTime, val text: String, val username: String) {}
--- src/main/kotlin/ServerOperations.kt
+++ src/main/kotlin/ServerOperations.kt
@@ -1,8 +1,66 @@
+import com.example.citylife.model.report.Report
+import org.bson.Document
+import kotlin.math.*
+
 class ServerOperations {
 
     val usersLocationAndDistance = MongoDB().readLocationForEveryUser()
 
-    fun retrieveLocationForEveryUser() {
-        val mapUserLocationDistance = emptyList<Map<String, String>>().toMutableList()
+    fun retrieveLocationForEveryUser(): MutableList<MutableMap<String, String>> {
+        val mapUserLocationDistance = emptyList<MutableMap<String, String>>().toMutableList()
+        usersLocationAndDistance.forEach {
+            document -> mapUserLocationDistance.add(returnMapFromDocument(document))
+        }
+
+        return mapUserLocationDistance
     }
+
+    fun interestedUsersForReport(report: Report) {
+        val reportLocation = report.location
+        val reportLatitude = reportLocation.split(" - ")[0]
+        val reportLongitude = reportLocation.split(" - ")[1]
+
+        retrieveLocationForEveryUser().filter {
+            entry -> (userInDistanceOfInterest(entry, reportLatitude, reportLongitude))
+
+        }.forEach {
+            entry -> send(entry["Username"]!!, report)
+        }
+    }
+
+    fun send(username: String, report: Report) {}
+
+    private fun returnMapFromDocument(document: Document): MutableMap<String, String> {
+        val mapUserLocationDistance = mutableMapOf<String, String>()
+        val entryList = document.entries.toList()
+        mapUserLocationDistance["Username"] = entryList[1].value.toString()
+        mapUserLocationDistance["Location"] = entryList[2].value.toString()
+        mapUserLocationDistance["Distance"] = entryList[3].value.toString()
+
+        return mapUserLocationDistance
+    }
+
+    private fun userInDistanceOfInterest(userMap: Map<String, String>, reportLat: String, reportLon: String): Boolean {
+        val userLatitude = userMap["Location"]?.split(" - ")?.get(0)?.toDouble()
+        val userLongitude = userMap["Location"]?.split(" - ")?.get(1)?.toDouble()
+        val userDistance = userMap["Distance"]?.toDouble()
+
+        return computeDistance(userLatitude, userLongitude, reportLat.toDouble(), reportLon.toDouble()) < userDistance!!
+
+    }
+
+    private fun computeDistance(userLatitude: Double?, userLongitude: Double?, reportLatitude: Double, reportLongitude: Double): Double {
+        val earthRadius = 6371
+        val dLat = deg2rad(reportLatitude - userLatitude!!)
+        val dLon = deg2rad(reportLongitude - userLongitude!!)
+        val a =
+            sin(dLat / 2) * sin(dLon / 2) +
+                    cos(deg2rad(reportLatitude)) * cos(deg2rad(userLatitude)) *
+                    sin(dLon / 2) * sin(dLon / 2)
+
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return earthRadius * c
+    }
+
+    private fun deg2rad(deg: Double) = deg * (PI / 180)
 }
