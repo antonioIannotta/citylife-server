6c30dd46d62ddd0a5d2512df16e31c4a9e73c5a3
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-12-16 23:19:49 +0100

    Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,3 @@
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-
 fun main() {
-    embeddedServer(Netty,port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, World!!")
-            }
-        }
-    }.start(wait = true)
+
 }
